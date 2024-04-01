5fe9e78b323ddeb3f846efc5d1764f0a35b2af4b
Mahmoud Abdo <mahmoud@example.com>
2024-04-01 05:31:39 +0200

    Solve Error

--- app/src/main/java/com/example/solutionx/App.kt
+++ app/src/main/java/com/example/solutionx/App.kt
@@ -7,6 +7,7 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App:Application() {
+
     override fun onCreate() {
         super.onCreate()
         LoggerProvider.provideLogger()
